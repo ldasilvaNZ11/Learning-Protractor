2cc83f92dc35f042f8453560e948131ff1b0aaef
ldasilvaNZ11 <lucas.cordeiro@gmail.com>
2017-01-24 22:49:44 +1300

    Update tm-spec.js
    adding a few comments to my tm-spec.js file

--- tm-spec.js
+++ tm-spec.js
@@ -1,16 +1,19 @@
+//tm-spec.js
 
+//the command below allows protractor to test non-AngularJS webpages
 browser.ignoreSynchronization = true;
+
 describe('TM front-page tests', function() {
 	it('TM front-page should have a logo', function () {
-//arrange
+//ARRANGE
 
 		browser.get('http://www.trademe.co.nz/');
 	
-//act
+//ACT
 //var tmLogo = element(by.id('SiteHeader_SiteTabs_kevin'));
-var tmLogo = $ ('#SiteHeader_SiteTabs_kevin');
+var tmLogo = $('#SiteHeader_SiteTabs_kevin');
 
-//verify
+//VERIFY
 expect(tmLogo.isDisplayed()).toBeTruthy();
 
     });
