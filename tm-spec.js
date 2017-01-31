a35228457bcc9fa5553e97e432849fd7ac8f4951
Lucas dev <lucasdev@Lucas-Da-Silva-MacBook-Pro.local>
2017-01-31 23:18:45 +1300

    added a test to check keyword search for trade me

--- tm-spec.js
+++ tm-spec.js
@@ -13,12 +13,29 @@ describe('TM front-page tests', function() {
 //ACT
 
 //var tmLogo = element(by.id('SiteHeader_SiteTabs_kevin'));
-var tmLogo = $('#SiteHeader_SiteTabs_kevin');
+        var tmLogo = $('#SiteHeader_SiteTabs_kevin');
+
 
 //ASSERT
-expect(tmLogo.isDisplayed()).toBeTruthy();
+		expect(tmLogo.isDisplayed()).toBeTruthy();
 
     });
+
+it ("Performing a keyword search", function() {
+    browser.get('http://www.trademe.co.nz/');
+
+    var searchBox = element(by.id('searchString'));
+    var searchResult = element(by.css('h1.inline-block'));
+
+    searchBox.sendKeys('nike');
+    searchBox.sendKeys(protractor.Key.ENTER)
+
+    expect(searchResult.getText()).toContain('Search results for \'nike\'');
+
+});
+
+
+
 
 });
 
