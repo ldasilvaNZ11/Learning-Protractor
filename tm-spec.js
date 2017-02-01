180b1eb75c7fbdccf6439a5124d0233aa75739d3
Lucas Da Silva <ldasilva@tmmac78.trademe.local>
2017-02-01 17:31:05 +1300

    trying out different scenarios to test keyword search

--- tm-spec.js
+++ tm-spec.js
@@ -25,13 +25,14 @@ it ("Performing a keyword search", function() {
     browser.get('http://www.trademe.co.nz/');
 
     var searchBox = element(by.id('searchString'));
-    var searchResult = element(by.css('h1.inline-block'));
+    var searchResult = element(by.css('inline-block'));
 
     searchBox.sendKeys('nike');
     searchBox.sendKeys(protractor.Key.ENTER)
 
-    expect(searchResult.getText()).toContain('Search results for \'nike\'');
-
+    //expect(searchResult.getText()).toContain('Search results for \'nike\'');
+    expect(browser.getCurrentUrl()).toBe('http://www.trademe.co.nz/Browse/SearchResults.aspx?searchString=nike&type=Search&searchType=all&user_region=100&user_district=0&generalSearch_keypresses=4&generalSearch_suggested=0&generalSearch_suggestedCategory=');
+    
 });
 
 
