a00ea5be8e0a87d4be8078eb811f2260c703d567
Lucas dev <lucasdev@Lucas-Da-Silva-MacBook-Pro.local>
2017-02-04 06:46:12 +1300

    keywordsearch tests are now working

--- tm-spec.js
+++ tm-spec.js
@@ -1,5 +1,7 @@
 //tm-spec.js
 
+var EC = protractor.ExpectedConditions;
+
 //the command below allows protractor to test non-AngularJS webpages
 browser.ignoreSynchronization = true;
 
@@ -21,22 +23,22 @@ describe('TM front-page tests', function() {
 
     });
 
-it ("Performing a keyword search", function() {
+fit ("Performing a keyword search", function() {
     browser.get('http://www.trademe.co.nz/');
 
     var searchBox = element(by.id('searchString'));
-    var searchResult = element(by.css('inline-block'));
+    var searchResult = element(by.css('h1.inline-block'));
 
     searchBox.sendKeys('nike');
-    searchBox.sendKeys(protractor.Key.ENTER)
+    searchBox.sendKeys(protractor.Key.ENTER);
+    //browser.sleep(3000);
+    browser.wait(EC.visibilityOf(searchResult), 3000);
 
-    //expect(searchResult.getText()).toContain('Search results for \'nike\'');
-    expect(browser.getCurrentUrl()).toBe('http://www.trademe.co.nz/Browse/SearchResults.aspx?searchString=nike&type=Search&searchType=all&user_region=100&user_district=0&generalSearch_keypresses=4&generalSearch_suggested=0&generalSearch_suggestedCategory=');
+    expect(searchResult.getText()).toContain('Search results for \'nike\'');
+    //expect(browser.getCurrentUrl()).toBe('http://www.trademe.co.nz/Browse/SearchResults.aspx?searchString=nike&type=Search&searchType=all&user_region=100&user_district=0&generalSearch_keypresses=4&generalSearch_suggested=0&generalSearch_suggestedCategory=');
     
 });
 
-
-
-
 });
+
 
